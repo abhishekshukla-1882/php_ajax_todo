d6ac64a8b365fc8e1c4efda7047b07806d3c7f6c
cedcoss <abhishekshukla25jancedcoss.com>
2022-02-21 19:22:54 +0530

    todo list

---
+++ unuseful/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Products
+	</title>
+	<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	<div id="wrapper">
+	<div id="header">
+		
+	</div>
+	<div id="main">
+		<div id="products">
+			
+		</div>
+		
+	</div>
+	<div id="footer">
+		<nav>
+			<ul id="footer-links">
+				<li><a href="#">Privacy</a></li>
+				<li><a href="#">Declaimers</a></li>
+			</ul>
+		</nav>
+	</div>
+</div>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
